94762fb2d7e7f4c508b6333876fce8acfe8a4f56
Luhm4nn <emyluhmann@gmail.com>
2025-07-08 17:01:58 -0300

    Update peliculas.controllers.js

--- Backend/Controllers/peliculas.controllers.js
+++ Backend/Controllers/peliculas.controllers.js
@@ -1,29 +1,40 @@
+import  prisma  from '../prisma/prisma.js';
+
 export const getPeliculas = async (req, res) => {
-    res.json({
-        message: 'Get Peliculas endpoint'
-    });
-}
+  const peliculas = await prisma.pelicula.findMany();
+  res.json(peliculas);
+};
 
 export const getPelicula = async (req, res) => {
-res.json({
-        message: 'Get Pelicula endpoint'
-    });
-}
+  const { id } = req.params;
+  const pelicula = await prisma.pelicula.findUnique({
+    where: { id: parseInt(id) }
+  });
+  res.json(pelicula);
+};
 
 export const createPelicula = async (req, res) => {
-res.json({
-        message: 'Create Pelicula endpoint'
-    });
-}
+  const { titulo, genero, duracion } = req.body;
+  const nueva = await prisma.pelicula.create({
+    data: { titulo, genero, duracion: parseInt(duracion) }
+  });
+  res.status(201).json(nueva);
+};
 
 export const deletePelicula = async (req, res) => {
-res.json({
-        message: 'Delete Pelicula endpoint'
-    });
-}
+  const { id } = req.params;
+  await prisma.pelicula.delete({
+    where: { id: parseInt(id) }
+  });
+  res.status(204).end();
+};
 
 export const updatePelicula = async (req, res) => {
-    res.json({
-        message: 'Update Pelicula endpoint'
-    });
-}
+  const { id } = req.params;
+  const { titulo, genero, duracion } = req.body;
+  const actualizada = await prisma.pelicula.update({
+    where: { id: parseInt(id) },
+    data: { titulo, genero, duracion: parseInt(duracion) }
+  });
+  res.json(actualizada);
+};
