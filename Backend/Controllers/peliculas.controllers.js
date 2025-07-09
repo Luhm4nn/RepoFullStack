0f92cd01bcdda4b0914665da8a169c695767578d
Luhm4nn <emyluhmann@gmail.com>
2025-07-09 12:18:55 -0300

    Solucion a problema con la ORM

--- Backend/Controllers/peliculas.controllers.js
+++ Backend/Controllers/peliculas.controllers.js
@@ -1,40 +1,22 @@
-import  prisma  from '../prisma/prisma.js';
+import prisma from "../prisma/prisma.js";
+// Controllers for Peliculas
 
 export const getPeliculas = async (req, res) => {
-  const peliculas = await prisma.pelicula.findMany();
-  res.json(peliculas);
+  res.json("Lista de Peliculas");
 };
 
 export const getPelicula = async (req, res) => {
-  const { id } = req.params;
-  const pelicula = await prisma.pelicula.findUnique({
-    where: { id: parseInt(id) }
-  });
-  res.json(pelicula);
+  res.json("Lista de Peliculas");
 };
 
 export const createPelicula = async (req, res) => {
-  const { titulo, genero, duracion } = req.body;
-  const nueva = await prisma.pelicula.create({
-    data: { titulo, genero, duracion: parseInt(duracion) }
-  });
-  res.status(201).json(nueva);
+  res.json("Lista de Peliculas");
 };
 
 export const deletePelicula = async (req, res) => {
-  const { id } = req.params;
-  await prisma.pelicula.delete({
-    where: { id: parseInt(id) }
-  });
-  res.status(204).end();
+  res.json("Lista de Peliculas");
 };
 
 export const updatePelicula = async (req, res) => {
-  const { id } = req.params;
-  const { titulo, genero, duracion } = req.body;
-  const actualizada = await prisma.pelicula.update({
-    where: { id: parseInt(id) },
-    data: { titulo, genero, duracion: parseInt(duracion) }
-  });
-  res.json(actualizada);
+ res.json("Lista de Peliculas");
 };
