cf39c65f54caf02ee2d01e7c6a2138653b0c45cb
Luhm4nn <emyluhmann@gmail.com>
2025-07-31 22:45:43 -0300

    Ajustes de documentos

--- Frontend/src/pages/MainPage.jsx
+++ Frontend/src/pages/MainPage.jsx
@@ -1,7 +1,10 @@
+import Navbar from "../components/navbar";
+
 function MainPage() {
   return (
     <div>
       <h1>Main Page</h1>
+      
       <p>Welcome to the main page!</p>
     </div>
   );
