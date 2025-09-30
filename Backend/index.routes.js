b5ede1361e7f4cbd33b7807fa9e0ea8258be19ea
Luhm4nn <emyluhmann@gmail.com>
2025-09-30 17:02:24 -0300

    refactor: reorganize route imports and usage in index.js and index.routes.js

--- Backend/index.routes.js
+++ Backend/index.routes.js
@@ -1,9 +1,25 @@
 import { Router } from "express";
+import { peliculasRoutes } from "./Peliculas/peliculas.routes.js";
+import { salasRoutes } from "./Salas/salas.routes.js";
+import { tarifasRoutes } from "./Tarifas/tarifas.routes.js";
+import { parametrosRoutes } from "./Parametros/parametros.routes.js";
+import { funcionesRoutes } from "./Funciones/funciones.routes.js";
+import { usuariosRoutes } from "./Usuario/usuarios.routes.js";
+import { authRoutes } from "./Auth/auth.routes.js";
 
 const router = Router();
+
 
 router.get("/", (req, res) => {
   res.send("Welcome to the Backend API");
 });
+
+router.use(peliculasRoutes);
+router.use(salasRoutes);
+router.use(tarifasRoutes);
+router.use(parametrosRoutes);
+router.use(funcionesRoutes);
+router.use(usuariosRoutes);
+router.use(authRoutes);
 
 export const indexRoutes = router;
