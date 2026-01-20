dd55938cf37d58e3dbdcb6bc5168bb7e05cdceba
Luhm4nn <emyluhmann@gmail.com>
2026-01-20 17:03:30 -0300

    Update cron job to run every 2 hours and finalize associated reservations

--- Backend/jobs/funcionesCron.js
+++ Backend/jobs/funcionesCron.js
@@ -12,26 +12,23 @@ export const iniciarCronFunciones = () => {
     return;
   }
 
-  // Corre cada 1 minuto para verificar funciones finalizadas
-  cron.schedule("*/1 * * * *", async () => {
+  // Corre cada 2 horas para verificar funciones finalizadas
+  cron.schedule("*/120 * * * *", async () => {
     try {
       const ahora = new Date();
-      // logger.debug(`[${ahora.toISOString()}] Verificando funciones finalizadas...`); // Reduced noise
 
-      // date range filter
-      const hace100Horas = new Date(ahora.getTime() - 100 * 60 * 60 * 1000);
       const dentro24Horas = new Date(ahora.getTime() + 24 * 60 * 60 * 1000);
 
       const funciones = await prisma.funcion.findMany({
         where: {
           estado: { not: 'Inactiva' },
           fechaHoraFuncion: {
-            gte: hace100Horas,
             lte: dentro24Horas,
           },
         },
         include: {
           pelicula: true,
+          reservas: true,
         },
       });
 
@@ -41,7 +38,7 @@ export const iniciarCronFunciones = () => {
         const fechaFin = new Date(
           funcion.fechaHoraFuncion.getTime() + funcion.pelicula.duracion * 60000
         );
-
+        const reservas = funcion.reservas;
         // if pelicula ended changes estado to Inactiva
         if (ahora > fechaFin) {
           await prisma.funcion.update({
@@ -53,14 +50,22 @@ export const iniciarCronFunciones = () => {
             },
             data: { estado: 'Inactiva' },
           });
-
+          
           funcionesActualizadas++;
           logger.info(
             `Función finalizada: ${funcion.pelicula.nombrePelicula} - Sala ${funcion.idSala} - ${funcion.fechaHoraFuncion.toLocaleString()}`
           );
+          for (const reserva of reservas) {
+            await prisma.reserva.update({
+              where: { idReserva: reserva.idReserva },
+              data: { estado: 'Finalizada' },
+            });
+          }
+          logger.info(
+            `Se finalizaron ${reservas.length} reservas asociadas a la función.`
+          );
         }
       }
-
       if (funcionesActualizadas > 0) {
         logger.info(`${funcionesActualizadas} función(es) marcada(s) como Inactiva`);
       }
@@ -69,5 +74,5 @@ export const iniciarCronFunciones = () => {
     }
   });
 
-  logger.info('Cron job de funciones iniciado - se ejecuta cada 1 minuto');
+  logger.info('Cron job de funciones iniciado - se ejecuta cada 2 horas');
 };
